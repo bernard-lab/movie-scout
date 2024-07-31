43eca478cec096810584c9300f8a850ff0d74f8c
Bernard <bernardacero@rocketmail.com>
2024-08-01 01:41:08 +0800

    updated api url

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 
 import MovieCard from "./components/MovieCard";
 
-const apiUrl = "http://www.omdbapi.com/?i=tt3896198&apikey=4d72c018";
+const apiUrl = process.env.REACT_APP_API_URL;
 
 const App = () => {
   const [movies, setMovies] = useState([]);
